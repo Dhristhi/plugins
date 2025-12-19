31b9edd9d956d4dd0997e404a8a8879a6afdf4a8
Harsh-T-Dhristhi <harsht@dhristhi.com>
2025-12-19 12:17:54 +0530

    added custom group layout

--- src/controls/renderers.js
+++ src/controls/renderers.js
@@ -4,12 +4,14 @@ import CustomFileUploadControl, {
 import CustomVerticalLayout, {
   customVerticalLayoutTester,
 } from "./CustomVerticalLayout";
+import CustomGroupLayout, { customGroupTester } from "./CustomGroupLayout";
 
 // Export all custom renderers with their testers
 export const customRenderers = [
   { tester: customFileUploadTester, renderer: CustomFileUploadControl },
+  { tester: customGroupTester, renderer: CustomGroupLayout },
   { tester: customVerticalLayoutTester, renderer: CustomVerticalLayout },
 ];
 
 // Export individual components for direct use if needed
-export { CustomFileUploadControl, CustomVerticalLayout };
+export { CustomFileUploadControl, CustomVerticalLayout, CustomGroupLayout };
