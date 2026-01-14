e1969fea38ecc8ea9efe3dd575300b2a5d9ba6b7
Parvez Shaikh <dhristhitechnologies@DHRPUN023.local>
2026-01-14 14:33:03 +0530

    chore: refactor code structure for improved readability and maintainability

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js';
+import globals from 'globals';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import globals from 'globals';
 import reactRefreshPlugin from 'eslint-plugin-react-refresh';
 
 export default [
