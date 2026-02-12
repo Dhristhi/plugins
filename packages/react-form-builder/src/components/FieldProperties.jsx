be4629ddf742a2715260e54bd7b03f5b077eeb69
madhavib-dhristhi <madhavib@dhristhi.com>
2026-02-12 16:20:06 +0530

    From array field remove the multi select and multi checkbox . (#124)
    * fix: checkbox issue resolved
    
    * fix: array of fieldtype fix
    
    ---------
    
    Co-authored-by: Madhavi Uttam Bhandawale <madhavib@DHRPUN001.local>

--- packages/react-form-builder/src/components/FieldProperties.jsx
+++ packages/react-form-builder/src/components/FieldProperties.jsx
@@ -589,9 +589,21 @@ const FieldProperties = ({ field, onFieldUpdate, fields, setFields, visibleField
     });
   };
 
-  const availableFieldTypes = defaultFieldTypes.filter(
-    (ft) => !ft.isLayout && visibleFields[ft.id] !== false
-  );
+  const availableFieldTypes = defaultFieldTypes.filter((ft) => {
+    if (ft.isLayout) return false;
+
+    // multiselect should depend on select field visibility
+    if (ft.id === 'multiselect') {
+      return visibleFields[ft.id] !== false && visibleFields['select'] !== false;
+    }
+
+    // multicheckbox should depend on checkbox field visibility
+    if (ft.id === 'multicheckbox') {
+      return visibleFields[ft.id] !== false && visibleFields['checkbox'] !== false;
+    }
+
+    return visibleFields[ft.id] !== false;
+  });
   const hasEnumOptions =
     ['select', 'radio', 'multiselect', 'multicheckbox'].includes(localField.type) ||
     (localField.schema?.type === 'array' &&
@@ -784,8 +796,12 @@ const FieldProperties = ({ field, onFieldUpdate, fields, setFields, visibleField
                   onChange={handleLayoutChange}
                   sx={layoutSelectSx}
                 >
-                  <MenuItem value="vertical-layout">{t('verticalLayout')}</MenuItem>
-                  <MenuItem value="horizontal-layout">{t('horizontalLayout')}</MenuItem>
+                  {visibleFields['vertical-layout'] && (
+                    <MenuItem value="vertical-layout">{t('verticalLayout')}</MenuItem>
+                  )}
+                  {visibleFields['horizontal-layout'] && (
+                    <MenuItem value="horizontal-layout">{t('horizontalLayout')}</MenuItem>
+                  )}
                 </Select>
               </FormControl>
             )}
